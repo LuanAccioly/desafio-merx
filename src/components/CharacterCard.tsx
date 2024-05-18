91c92a633bac223fbb3b613b49d879018af397e7
Luan Accioly <acciolyluan@gmail.com>
2024-05-18 00:45:59 -0300

    feat: create CharacterCard component for displaying character information

---
+++ src/components/CharacterCard.tsx
@@ -0,0 +1,24 @@
+import { CharacterData } from "@/types/characters"
+import { Card, Image } from "antd"
+import Meta from "antd/es/card/Meta"
+
+interface CharacterProps {
+character: CharacterData;
+
+}
+
+const CharacterCard: React.FC<CharacterProps> = ({ character }) => {
+  return (
+    <Card
+    hoverable
+    style={{ width: 240 }}
+    cover={
+        <Image style={{objectFit: "cover"}} height={250} preview={false} alt="example" src={character.thumbnail.path + "." + character.thumbnail.extension} />
+  }
+  >
+    <Meta title={character.name} description={"#" + character.id} />
+  </Card>
+  )
+}
+
+export { CharacterCard };
