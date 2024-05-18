7063507832eae3a6129147814d54f57d2e72729e
Luan Accioly <acciolyluan@gmail.com>
2024-05-18 03:03:47 -0300

    feat: add ignore incomplete characters functionality to Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Flex, Image, Input, Layout } from "antd";
+import { Flex, Image, Input, Layout, Switch } from "antd";
 import { Content, Header } from "antd/es/layout/layout";
 import api from "@/services/api";
 import { CharacterCard } from "@/components/CharacterCard";
@@ -12,17 +12,30 @@ import {
 import { CharacterData } from "@/types/characters";
 import { useEffect, useState } from "react";
 import Search from "antd/es/input/Search";
-import { get } from "http";
+import { Space, Typography } from "antd";
 
 export const HomePage = () => {
   const marvelLogo = "/marvel_logo.svg";
 
   const [characters, setCharacters] = useState<CharacterData[]>([]);
   const [search, setSearch] = useState("");
+  const [ignoreIncomplete, setIgnoreIncomplete] = useState(false);
+  const { Text } = Typography;
 
   useEffect(() => {
     getCharacters();
   }, []);
+
+  useEffect(() => {
+    if (ignoreIncomplete) {
+      const filteredCharacters = characters.filter(
+        (character) => !character.thumbnail.path.includes("image_not_available")
+      );
+      setCharacters(filteredCharacters);
+    } else {
+      getCharacters(search);
+    }
+  }, [ignoreIncomplete]);
 
   async function getCharacters(name?: string) {
     const params: { [key: string]: string | number | undefined } = {
@@ -39,10 +52,13 @@ export const HomePage = () => {
     const response = await api.get("/characters", { params });
     const characters: CharacterData[] = response.data.data.results;
     setCharacters(characters);
-    // return characters;
   }
   function handleSearch() {
     getCharacters(search);
+  }
+
+  function handleIgnoreIncomplete() {
+    setIgnoreIncomplete((prev) => !prev);
   }
 
   return (
@@ -64,10 +80,31 @@ export const HomePage = () => {
           enterButton
         />
       </Header>
-      <Flex gap={20} justify="center" align="center" wrap="wrap">
-        {characters.map((character) => (
-          <CharacterCard character={character} key={character.id} />
-        ))}
+      <Flex
+        style={{
+          flexDirection: "column",
+          padding: "0px 40px",
+        }}
+        justify="flex-end"
+        align="end"
+        gap="20px"
+      >
+        <Flex
+          style={{ width: "300px", paddingRight: "10px" }}
+          gap="10px"
+          justify="flex-end"
+        >
+          <Text>Ignorar incompletos</Text>
+          <Switch
+            checked={ignoreIncomplete}
+            onChange={handleIgnoreIncomplete}
+          />
+        </Flex>
+        <Flex gap={20} justify="center" align="center" wrap="wrap">
+          {characters.map((character) => (
+            <CharacterCard character={character} key={character.id} />
+          ))}
+        </Flex>
       </Flex>
     </Layout>
   );
