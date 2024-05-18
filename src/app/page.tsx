34cff1826d80c97a004956841b3afee5d6ee797b
Luan Accioly <acciolyluan@gmail.com>
2024-05-18 00:46:59 -0300

    refactor: update page component to use HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,7 @@
-'use client'
-
-import Image from "next/image";
-import { Button } from "antd";
-import { use, useEffect } from "react";
-import api from "@/services/api";
-
-async function getHeroes(){
-  try{
-    const response = await api.get('/characters', {
-      params: {
-        ts: 1,
-        apikey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
-        hash: process.env.NEXT_PUBLIC_HASH,
-      }
-    });
-    console.log(response.data.data.results);
-  }catch(error){
-    console.log(error);
-  }
-}
+import { HomePage } from "@/pages/Home";
 
 export default function Home() {
-
-  useEffect(() => {
-    getHeroes();
-  }, []);
-  
   return (  
-    <Button type="primary">botão</Button>
+    <HomePage />
   );
 }
