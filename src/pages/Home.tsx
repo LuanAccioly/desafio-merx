a7c56569ab3f2d199a8053c3a339f737edbece5e
Luan Accioly <acciolyluan@gmail.com>
2024-05-18 01:59:49 -0300

    feat: add search functionality to Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Flex, Layout } from "antd";
+import { Flex, Image, Input, Layout } from "antd";
 import { Content, Header } from "antd/es/layout/layout";
 import api from "@/services/api";
 import { CharacterCard } from "@/components/CharacterCard";
@@ -11,30 +11,59 @@ import {
 } from "next";
 import { CharacterData } from "@/types/characters";
 import { useEffect, useState } from "react";
+import Search from "antd/es/input/Search";
+import { get } from "http";
 
-async function getCharacters() {
-  const response = await api.get("/characters", {
-    params: {
+export const HomePage = () => {
+  const marvelLogo = "/marvel_logo.svg";
+
+  const [characters, setCharacters] = useState<CharacterData[]>([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
+
+  async function getCharacters(name?: string) {
+    const params: { [key: string]: string | number | undefined } = {
       limit: 100,
       ts: 1,
       apikey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
       hash: process.env.NEXT_PUBLIC_HASH,
-    },
-  });
-  const characters: CharacterData[] = response.data.data.results;
-  return characters;
-}
+    };
 
-export const HomePage = () => {
-  const [characters, setCharacters] = useState<CharacterData[]>([]);
+    if (name) {
+      params.nameStartsWith = name;
+    }
 
-  useEffect(() => {
-    getCharacters().then((data) => setCharacters(data));
-  }, []);
+    const response = await api.get("/characters", { params });
+    const characters: CharacterData[] = response.data.data.results;
+    setCharacters(characters);
+    // return characters;
+  }
+  function handleSearch() {
+    getCharacters(search);
+  }
 
   return (
     <Layout>
-      <Header>opa</Header>
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          backgroundColor: "transparent",
+          gap: 20,
+        }}
+      >
+        <Image height={40} src={marvelLogo} alt="marvel logo" preview={false} />
+        <Search
+          placeholder="Busque por um personagem"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          onSearch={handleSearch}
+          enterButton
+        />
+      </Header>
       <Flex gap={20} justify="center" align="center" wrap="wrap">
         {characters.map((character) => (
           <CharacterCard character={character} key={character.id} />
