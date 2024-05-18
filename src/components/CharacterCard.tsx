e5e1fbaf884d2e71b72f01ffc19b1f015bd43791
Luan Accioly <acciolyluan@gmail.com>
2024-05-18 01:59:29 -0300

    refactor: optimize CharacterCard component rendering

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,24 +1,29 @@
-import { CharacterData } from "@/types/characters"
-import { Card, Image } from "antd"
-import Meta from "antd/es/card/Meta"
+import { CharacterData } from "@/types/characters";
+import { Card, Image } from "antd";
+import Meta from "antd/es/card/Meta";
 
 interface CharacterProps {
-character: CharacterData;
-
+  character: CharacterData;
 }
 
 const CharacterCard: React.FC<CharacterProps> = ({ character }) => {
   return (
     <Card
-    hoverable
-    style={{ width: 240 }}
-    cover={
-        <Image style={{objectFit: "cover"}} height={250} preview={false} alt="example" src={character.thumbnail.path + "." + character.thumbnail.extension} />
-  }
-  >
-    <Meta title={character.name} description={"#" + character.id} />
-  </Card>
-  )
-}
+      hoverable
+      style={{ width: 240 }}
+      cover={
+        <Image
+          style={{ objectFit: "cover" }}
+          height={250}
+          preview={false}
+          alt={character.name}
+          src={character.thumbnail.path + "." + character.thumbnail.extension}
+        />
+      }
+    >
+      <Meta title={character.name} description={"#" + character.id} />
+    </Card>
+  );
+};
 
 export { CharacterCard };
