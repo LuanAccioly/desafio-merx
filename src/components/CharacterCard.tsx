93f900564940b3e1dcc4ba1e4cc7e570d75a0247
Luan Accioly <acciolyluan@gmail.com>
2024-05-19 16:07:31 -0300

    refactor: add responsive design and loading state on CharacterCard

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,11 +1,14 @@
 import { getComics } from "@/app/actions";
 import { CharacterData } from "@/types/characters";
 import { ComicData } from "@/types/comics";
-import { Card, Carousel, Flex, Image, Modal } from "antd";
+import { Card, Carousel, Flex, Image, Modal, Spin } from "antd";
 import Meta from "antd/es/card/Meta";
 import Paragraph from "antd/es/typography/Paragraph";
 import Title from "antd/es/typography/Title";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { CSSProperties } from "react";
+import { Grid } from "antd";
+import { get } from "http";
 
 interface CharacterProps {
   character: CharacterData;
@@ -15,8 +18,35 @@ interface ComicProps {
   characterID: number;
 }
 
+type StylesType = {
+  Input: CSSProperties;
+  OtherComponent: CSSProperties;
+};
+
 const CharacterCard: React.FC<CharacterProps> = ({ character }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const { useBreakpoint } = Grid;
+
+  // Change the slidesToShow according to the screen size
+  function getFlexOrientation(screen: any) {
+    if (screen.md) {
+      return "row";
+    }
+    if (screen.sm) {
+      return "row";
+    }
+    return "column";
+  }
+
+  function getAlignItems(screen: any) {
+    if (screen.md) {
+      return "flex-start";
+    }
+    if (screen.sm) {
+      return "center";
+    }
+    return "center";
+  }
 
   const showModal = () => {
     setIsModalOpen(true);
@@ -34,7 +64,7 @@ const CharacterCard: React.FC<CharacterProps> = ({ character }) => {
         cover={
           <Image
             style={{ objectFit: "cover" }}
-            height={250}
+            height={"50%"}
             preview={false}
             alt={character.name}
             src={character.thumbnail.path + "." + character.thumbnail.extension}
@@ -51,15 +81,22 @@ const CharacterCard: React.FC<CharacterProps> = ({ character }) => {
         width={800}
         centered
       >
-        <Flex style={{ flexDirection: "row" }}>
+        <Flex
+          style={{
+            flexDirection: getFlexOrientation(useBreakpoint()),
+            margin: "10 0",
+            gap: 10,
+          }}
+          align={getAlignItems(useBreakpoint())}
+        >
           <Image
             style={{ objectFit: "cover", borderRadius: 10 }}
-            width={300}
-            height={300}
+            width={"40%"}
+            height={"50%"}
             alt={character.name}
             src={character.thumbnail.path + "." + character.thumbnail.extension}
           />
-          <Paragraph style={{ flex: 1, margin: 10, fontSize: 16 }}>
+          <Paragraph style={{ flex: 1, fontSize: 16 }}>
             {character.description
               ? character.description
               : "No description available"}
@@ -73,11 +110,25 @@ const CharacterCard: React.FC<CharacterProps> = ({ character }) => {
 
 const ComicsCarousel: React.FC<ComicProps> = ({ characterID }) => {
   const [comics, setComics] = useState<ComicData[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const { useBreakpoint } = Grid;
+
+  // Change the slidesToShow according to the screen size
+  function getSlidesToShow(screen: any) {
+    if (screen.md) {
+      return 3;
+    }
+    if (screen.sm) {
+      return 2;
+    }
+    return 1;
+  }
 
   useEffect(() => {
     const getData = async () => {
       const data = await getComics(characterID);
       setComics(data);
+      setIsLoading(false);
     };
     getData();
   }, []);
@@ -86,22 +137,25 @@ const ComicsCarousel: React.FC<ComicProps> = ({ characterID }) => {
     <Flex
       style={{
         flexDirection: "column",
-        marginTop: "20px",
+        marginTop: "30px",
         borderRadius: 10,
-        padding: 10,
       }}
     >
-      <Title style={{ marginTop: 10 }} level={4}>
-        Comics
-      </Title>
+      <Title level={4}>Comics</Title>
+
+      {isLoading && <Spin />}
+      {!isLoading && comics.length === 0 && (
+        <Paragraph>No comics available</Paragraph>
+      )}
       <Carousel
         style={{
           width: "100%",
           height: "400px",
+          gap: 10,
         }}
         autoplay={true}
         draggable
-        slidesToShow={3}
+        slidesToShow={getSlidesToShow(useBreakpoint())}
         arrows
         infinite={false}
       >
@@ -114,11 +168,26 @@ const ComicsCarousel: React.FC<ComicProps> = ({ characterID }) => {
 };
 
 const Comic = (comic: ComicData) => {
+  const { useBreakpoint } = Grid;
+
+  function getComicWidth(screen: any) {
+    if (screen.md) {
+      return "80%";
+    }
+    if (screen.sm) {
+      return "60%";
+    }
+    return "100%";
+  }
+
   return (
     <Card
       key={comic.id}
       // hoverable
-      style={{ width: 200, height: 380, margin: 10 }}
+      style={{
+        width: getComicWidth(useBreakpoint()),
+        height: 380,
+      }}
       cover={
         <Image
           style={{ objectFit: "cover" }}
