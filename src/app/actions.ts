a92f43b6996fc283b4929bf04fbb19c574e0f5ca
Luan Accioly <acciolyluan@gmail.com>
2024-05-19 01:10:16 -0300

    feat: add getComics function to actions.ts

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,6 +2,7 @@
 
 import api from "@/services/api";
 import { CharacterData } from "@/types/characters";
+import { ComicData } from "@/types/comics";
 
 export async function getCharacters(name?: string): Promise<CharacterData[]> {
   const params: { [key: string]: string | number | undefined } = {
@@ -18,4 +19,19 @@ export async function getCharacters(name?: string): Promise<CharacterData[]> {
   const response = await api.get("/characters", { params });
   const characters: CharacterData[] = response.data.data.results;
   return characters as CharacterData[];
+}
+
+export async function getComics(characterId: number): Promise<ComicData[]> {
+  const params: { [key: string]: string | number | undefined } = {
+    limit: 8,
+    ts: 1,
+    apikey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+    hash: process.env.NEXT_PUBLIC_HASH,
+  };
+
+  const response = await api.get(`/characters/${characterId}/comics`, {
+    params,
+  });
+  const characters: ComicData[] = response.data.data.results;
+  return characters as ComicData[];
 }
